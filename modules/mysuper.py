77f420af145f9fc216b3d1058bf980ca2e7876c1
michaelMatve <michael.matve@gmail.com>
2023-01-15 19:03:24 +0200

    mysuper 3-tier

---
+++ modules/mysuper.py
@@ -0,0 +1,26 @@
+import requests
+
+class MySuper:
+    def __init__(self,Super_Id) -> None:
+        self.Super_Id = Super_Id
+
+    def get_super(self):
+        URL = "http://172.17.0.3:8000/mysuper/getsuper"
+        PARAM = {"Super_Id":self.Super_Id}
+        r = requests.get(url = URL, params=PARAM)
+        data = r.json()
+        return data
+    
+    def set_super(self,super_name:str, super_city:str):
+        URL = "http://172.17.0.3:8000/mysuper/setsuper"
+        PARAM = {"Super_Id":self.Super_Id, "super_name":super_name , "super_city":super_city}
+        r = requests.get(url = URL, params=PARAM)
+        data = r.json()
+        return data
+
+    def moveSuper(self, old_city:str,new_city:str):
+        URL = "http://172.17.0.3:8000/mysuper/movesuper"
+        PARAM = {"old_city":old_city, "new_city":new_city, "Super_Id":self.Super_Id}
+        r = requests.get(url = URL, params=PARAM)
+        data = r.json()
+        return data
--- src/main.py
+++ src/main.py
@@ -6,8 +6,38 @@ from threading import Thread
 import asyncio
 from modules.signup import SignUp
 from modules.signin import SignIn
+from modules.mysuper import MySuper
 
 app = FastAPI()
+
+# //////////////////////michael////////////////////////////
+@app.get("/signin")
+async def get_sighin_user(username:str, password:str):
+    print("11")
+    mySingIn = SignIn(username, password)
+    print("22")
+    stam =  mySingIn.dosighin()
+    print("33")
+    return stam
+
+@app.get("/mysuper/getsuper")
+async def get_super_info(Super_Id:str):
+    tempMySuper = MySuper(Super_Id)
+    stam =  tempMySuper.get_super()
+    return stam
+
+@app.get("/mysuper/setsuper")
+async def set_super_info(Super_Id:str, super_name:str, super_city:str):
+    tempMySuper = MySuper(Super_Id)
+    old_super = tempMySuper.get_super()
+    stam =  tempMySuper.set_super(super_name,super_city)
+    if super_city != old_super.get("super_city") :
+        tempMySuper.moveSuper(old_super.get("super_city"),super_city)
+
+    return stam
+
+    
+# //////////////////////michael////////////////////////////
 
 
 # http://localhost:5000/
@@ -59,15 +89,6 @@ async def get_city(super_ID:str, super_name:str, super_city:str, comments_size:s
     insertion_status = sign_up.insert_city_to_fb()
     
     return insertion_status
-
-@app.get("/signin")
-async def get_sighin_user(username:str, password:str):
-    print("11")
-    mySingIn = SignIn(username, password)
-    print("22")
-    stam =  mySingIn.dosighin()
-    print("33")
-    return stam
 
 
 
