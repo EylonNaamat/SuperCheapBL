8c69b123d325978d4db83328d1b1777f20b56dcc
EylonNaamat <eylonnaamat@gmail.com>
2023-01-16 20:14:54 +0200

    main with display super

--- src/main.py
+++ src/main.py
@@ -6,8 +6,59 @@ from threading import Thread
 import asyncio
 from modules.signup import SignUp
 from modules.signin import SignIn
+from modules.mysuper import MySuper
+from modules.myaccount import MyAccount
+from modules.addcomment import AddComment
+from modules.InsertItem import InsertItem
+from modules.DeleteItem import DeleteItem
+from modules.display_super import DisplaySuper
 
 app = FastAPI()
+
+# //////////////////////michael////////////////////////////
+@app.get("/signin")
+async def get_sighin_user(username:str, password:str):
+    print("11")
+    mySingIn = SignIn(username, password)
+    print("22")
+    stam =  mySingIn.dosighin()
+    print("33")
+    return stam
+
+@app.get("/mysuper/getsuper")
+async def get_super_info(Super_Id:str):
+    tempMySuper = MySuper(Super_Id)
+    stam =  tempMySuper.get_super()
+    return stam
+
+@app.get("/mysuper/setsuper")
+async def set_super_info(Super_Id:str, super_name:str, super_city:str):
+    tempMySuper = MySuper(Super_Id)
+    old_super = tempMySuper.get_super()
+    stam =  tempMySuper.set_super(super_name,super_city)
+    if super_city != old_super.get("super_city") :
+        tempMySuper.moveSuper(old_super.get("super_city"),super_city)
+
+    return stam
+
+@app.get("/myaccount/setuser")
+async def my_acc_set_user(first_name:str, last_name:str, email:str, username:str, password:str, city:str, birth_date:str, gender:str, is_manager:str, super_id:str):
+    temp_account = MyAccount({'first_name':first_name, 'last_name':last_name, 'email':email, 'username':username,
+            'password':password, 'city':city, 'birth_date':birth_date, 'gender':gender, 'is_manager':is_manager,
+            'super_id':super_id})
+    stam = temp_account.change_user()
+    return stam
+
+@app.get("/addcomment")
+async def add_comment(id_comment:str, super_name:str, super_city:str, user_username:str, grade, review:str):
+    tempaddComment = AddComment({'id_comment':id_comment, 'super_name':super_name, 'super_city':super_city, 'user_username':user_username,
+            'grade':grade, 'review':review})
+    stam =  tempaddComment.addCommentToDataBase()
+    return stam
+    
+
+    
+# //////////////////////eylon////////////////////////////
 
 
 # http://localhost:5000/
@@ -60,30 +111,36 @@ async def get_city(super_ID:str, super_name:str, super_city:str, comments_size:s
     
     return insertion_status
 
-@app.get("/signin")
-async def get_sighin_user(username:str, password:str):
-    print("11")
-    mySingIn = SignIn(username, password)
-    print("22")
-    stam =  mySingIn.dosighin()
-    print("33")
-    return stam
+# http://localhost:5000/displaysuper?city=ariel&itemlist=bira_3-corona,shampoo-dove
+@app.get("/displaysuper")
+async def get_supers(city:str, itemlist:str):
+    
+    display_super = DisplaySuper(city, itemlist)
+
+    to_display = display_super.get_supers()
+    
+    return to_display
 
 
+#///////////////// Ben/////////////////////////////
+@app.get("/addItem")
+async def additem(itemname:str, price:str, company:str,super_id:str):
+    ITEM = {'item_name':itemname, 'price':price, 'company':company,'super_id':super_id}
+    
+    insert_item = InsertItem(ITEM)
 
-@app.get("/items")
-def read_item(item_id: int, q: Union[str, None] = None):
+    insertion_status = insert_item.insert_item_to_FB()
 
-    # api-endpoint
-    URL = "http://172.17.0.3:8000/items"
+    return insertion_status
 
-    # defining a params dict for the parameters to be sent to the API
-    PARAMS = {'item_id':item_id, 'q':q}
-  
-    # sending get request and saving the response as response object
-    r = requests.get(url = URL, params=PARAMS)
-  
-    # extracting data in json format
-    data = r.json()
-    # print(data)
-    return data
+@app.get("/deleteItem")
+async def additem(itemname:str, company:str,super_id:str):
+    ITEM = {'item_name':itemname, 'company':company,'super_id':super_id}
+    
+    delete_item = DeleteItem(ITEM)
+
+    insertion_status = delete_item.delete_item_from_DB()
+
+    return insertion_status
+
+    #///////////////////////////////////////////////////
